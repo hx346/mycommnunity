c33ec67c60f5ec3a1bfb24f12a8fbb8c3486bb8f
lox kdk <hexin@loxdeMacBook-Pro.local>
2019-09-27 14:43:24 +0800

    init repo

---
+++ src/main/java/com/example/mycommunity/MycommunityApplication.java
@@ -0,0 +1,13 @@
+package com.example.mycommunity;
+//启动入口
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MycommunityApplication.class, args);
+    }
+
+}

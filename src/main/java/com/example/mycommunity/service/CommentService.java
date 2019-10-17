be9c101dbb2bb972146989cc9c86397a2afc1b4e
hexin <hhhhh@oo.com>
2019-10-17 20:20:24 +0800

    增加回复问题功能

--- src/main/java/com/example/mycommunity/service/CommentService.java
+++ src/main/java/com/example/mycommunity/service/CommentService.java
@@ -10,6 +10,7 @@ import com.example.mycommunity.model.Comment;
 import com.example.mycommunity.model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CommentService {
@@ -20,6 +21,8 @@ public class CommentService {
     @Autowired
     private QuestionExtMapper questionExtMapper;
 
+    //控制事务
+    @Transactional
     public void insert(Comment comment) {
         if (comment.getParentId() == null || comment.getParentId() == 0) {
             throw new CustomizeException(CustomizeErrorCode.TARGET_PARAM_NOT_FOUND);
